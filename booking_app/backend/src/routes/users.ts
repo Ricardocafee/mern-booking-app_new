6f72822f6d30af46c534cc538d0a5b0f80b96e40
Ricardocafee <ricardocafee00@gmail.com>
2024-02-10 10:53:58 +0000

    User registration done

--- booking_app/backend/src/routes/users.ts
+++ booking_app/backend/src/routes/users.ts
@@ -34,7 +34,7 @@ router.post("/register", [
         secure: process.env.NODE_ENV === "production",
         maxAge: 8640000,
     })
-    return res.sendStatus(200);
+    return res.status(200).send({ message: "User registered OK" });
     }catch (error) {
         console.log(error);
         res.status(500).send({message: "Something went wrong"});
