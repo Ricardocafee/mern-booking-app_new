e13984cb857e7540e728fe5d6452830f1174bb59
Ricardocafee <ricardocafee00@gmail.com>
2024-04-08 22:47:15 +0100

    JSON fixed

--- booking_app/frontend/src/components/MapDisplay.tsx
+++ booking_app/frontend/src/components/MapDisplay.tsx
@@ -1,85 +1,133 @@
-import { useEffect, useState } from 'react'
+import React, { FC } from 'react';
 
-import MapPicker from 'react-google-map-picker'
+function isGoogleMapScriptLoaded(id: string): boolean {
+    const scripts: HTMLCollectionOf<HTMLScriptElement> = document.head.getElementsByTagName('script');
+    for (let i: number = 0; i < scripts.length; i++) {
+        if (scripts[i].getAttribute('id') === id) {
+            return true;
+        }
+    }
 
+    return false;
+}
 
-const DefaultLocation = { lat: 10, lng: 106};
-const DefaultZoom = 8;
+function loadScript(src: string, id: string) {
+    if (isGoogleMapScriptLoaded(id)) {
+        // Make sure the script is loaded
+        return new Promise((resolve) => setTimeout(resolve, 500));
+    }
+
+    const script = document.createElement('script');
+    script.setAttribute('async', '');
+    script.setAttribute('id', id);
+    script.src = src;
+    (document.querySelector('head') as any).appendChild(script);
+
+    return new Promise<void>((resolve) => {
+        script.onload = () => {
+            resolve();
+        };
+    });
+}
+
+type Location = {
+    lat: number,
+    lng: number
+}
 
 enum MapTypeId {
-  Roadmap = "roadmap",
-  Satellite = "satellite",
-  Hybrid = "hybrid",
-  Terrain = "terrain"
+    Roadmap = 'roadmap',
+    Satellite = 'satellite',
+    Hybrid = 'hybrid',
+    Terrain = 'terrain'
 }
 
-const TypeMap: MapTypeId = MapTypeId.Roadmap;
+type Props = {
+    apiKey: string,
+    defaultLocation: Location;
+    zoom?: number;
+    onChangeLocation?(lat: number, lng: number): void;
+    onChangeZoom?(zoom: number): void;
+    style?: any;
+    className?: string;
+    mapTypeId?: MapTypeId
+}
 
-const MapDisplay = () => {
+function isValidLocation(location: Location) {
+    return location && Math.abs(location.lat) <= 90 && Math.abs(location.lng) <= 180;
+}
 
-  const [defaultLocation, setDefaultLocation] = useState(DefaultLocation);
+const GOOGLE_SCRIPT_URL = 'https://maps.googleapis.com/maps/api/js?libraries=places&key=';
 
-  const [location, setLocation] = useState(defaultLocation);
-  const [zoom, setZoom] = useState(DefaultZoom);
-  const typemap = TypeMap;
+const MapPicker: FC<Props> = ({ apiKey, defaultLocation, zoom = 7, onChangeLocation, onChangeZoom, style, className, mapTypeId }) => {
+    const MAP_VIEW_ID = 'google-map-view-' + Math.random().toString(36).substr(2, 9);
+    const map = React.useRef<any>(null);
+    const marker = React.useRef<any>(null);
 
-  const [apiLoaded, setApiLoaded] = useState(false);
+    function handleChangeLocation() {
+        if (onChangeLocation) {
+            const currentLocation = marker.current.getPosition();
+            onChangeLocation(currentLocation.lat(), currentLocation.lng());
+        }
+    }
 
+    function handleChangeZoom() {
+        onChangeZoom && onChangeZoom(map.current.getZoom());
+    }
 
-  useEffect(() => {
-    // Here you can perform any asynchronous operations required for map initialization,
-    // such as loading Google Maps API script
-    // Ensure that the necessary resources are loaded before setting apiLoaded to true
+    function loadMap() {
+        const Google = (window as any).google;
+        const validLocation = isValidLocation(defaultLocation) ? defaultLocation : { lat: 0, lng: 0 };
 
-    // For demonstration purpose, setting apiLoaded to true after 2 seconds
-    const timeout = setTimeout(() => {
-      setApiLoaded(true);
-    }, 500);
+        map.current = new Google.maps.Map(document.getElementById(MAP_VIEW_ID),
+            {
+                center: validLocation,
+                zoom: zoom,
+                ...(mapTypeId && { mapTypeId })
+            });
 
-    // Clean up the timeout to avoid memory leaks
-    return () => clearTimeout(timeout);
-  }, []); // Empty dependency array ensures this effect runs only once after initial render
+        if (!marker.current) {
+            marker.current = new Google.maps.Marker({
+                position: validLocation,
+                map: map.current,
+                draggable: true
+            });
+            Google.maps.event.addListener(marker.current, 'dragend', handleChangeLocation);
+        } else {
+            marker.current.setPosition(validLocation);
+        }
 
-  function handleChangeLocation (lat: number, lng: number){
-    setLocation({lat:lat, lng:lng});
-  }
-  
-  function handleChangeZoom (newZoom:number){
-    setZoom(newZoom);
-  }
+        map.current.addListener('click', function (event: any) {
+            const clickedLocation = event.latLng;
 
-  function handleResetLocation(){
-    setDefaultLocation({ ... DefaultLocation});
-    setZoom(DefaultZoom);
-  }
+            marker.current.setPosition(clickedLocation);
+            handleChangeLocation();
+        });
 
-  if(!apiLoaded) {
+        map.current.addListener('zoom_changed', handleChangeZoom);
+    }
+
+    React.useEffect(() => {
+        loadScript(GOOGLE_SCRIPT_URL + apiKey, 'google-maps-' + apiKey).then(loadMap);
+    }, [])  // eslint-disable-line react-hooks/exhaustive-deps
+
+    React.useEffect(() => {
+        if (marker.current) {
+            map.current.setCenter(defaultLocation);
+            marker.current.setPosition(defaultLocation);
+        }
+    }, [defaultLocation]);
+
+    React.useEffect(() => {
+        if (map.current) {
+            map.current.setZoom(zoom);
+        }
+    }, [zoom]);
+
+    const componentStyle = Object.assign({ width: '100%', height: '600px' }, style || {});
+
     return (
-      <div>Loading...
-      </div>
-      
-    )
-  }
-
-  return (
-    <>
-  <button onClick={handleResetLocation}>Reset Location</button>
-  <label>Latitute:</label><input type='text' value={location.lat} disabled/>
-  <label>Longitute:</label><input type='text' value={location.lng} disabled/>
-  <label>Zoom:</label><input type='text' value={zoom} disabled/>
-
-  <div id='map' className='items-center' style={{ border: '1px solid #ccc', padding: '5px', borderRadius: '5px'}}>
-  <MapPicker defaultLocation={defaultLocation}
-    zoom={zoom}
-    mapTypeId={typemap}
-    style={{height:'700px'}}
-    onChangeLocation={handleChangeLocation} 
-    onChangeZoom={handleChangeZoom}
-    apiKey='AIzaSyBG0qUTBcs3wxiJwvEEe5hKa7QEYYkPd2Y'
-    />
-    </div>
-  </>
-  );
-}
-
-export default MapDisplay;
+        <div id={MAP_VIEW_ID} style={componentStyle} className={className}></div>
+    );
+};
+export default MapPicker;
--- booking_app/frontend/src/forms/ManagePropertyForm/MapSection.tsx
+++ booking_app/frontend/src/forms/ManagePropertyForm/MapSection.tsx
@@ -4,12 +4,16 @@ import MapDisplay from "../../components/MapDisplay";
 const MapSection = () => {
 
     // State variables to hold selected latitude and longitude
+
+    const apiKey = 'AIzaSyBG0qUTBcs3wxiJwvEEe5hKa7QEYYkPd2Y';
+    const defaultLocation = { lat: 0, lng: 0 }; // Default location example
+
    
     return (
         <div>
             <h2 className="text-2xl font-bold mb-3">Map Location</h2>
             
-            <MapDisplay/>
+            <MapDisplay apiKey={apiKey} defaultLocation={defaultLocation} />
             
         </div>
     );
